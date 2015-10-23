4a63514bad69db96cd81af6aa17d8049300b2bcb
jauzmendiji <jauzmendiji@github.com>
2015-10-23 09:08:31 +0200

    inportatzeko

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-02 06:42:47 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-10-02T06:42:47+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-10-02T06:42:50+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-10-02T06:42:53+00:00	INFO	joomlafailure	Empty password not allowed
